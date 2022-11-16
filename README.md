4b35484811e86f19d3f3563806ef50a502ba5512
starkvic <starkvic@github.com>
2022-11-16 17:09:27 +0300

    Dates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Cloud-Skills-Challenge
+This is my step by step journey following Microsoft Learn's Cloud Skills Challenge.
---
+++ dates.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+#Printing today's date
+print(date.today())
+
+#converting the date into string using data type conversion then printing it
+print("The date today is "+str(date.today()))
